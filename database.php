0be15f333da101e9a73dba6ea9c5dc9672c8d274
kopreshikanita-cyber <kopreshikanita@gmail.com>
2026-05-03 23:55:05 +0200

    databaza

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database{
+    private $host = 'localhost';
+    private $dbname = 'projekti';
+    private $username = 'user1';
+    private $password = 'kanitaAdea123';
+    private $conn;
+
+    public function __construct(){
+        try{
+            $this->conn = new PDO(dsn: "mysql:host={$this->host};dbname={$this->dbname}", username: $this->username, password: $this->password);
+            $this->conn->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            die("Connection failed: ". $e->getMessage());
+        }
+    }
+    public function getConnection(){
+            return $this->conn;
+}
+}
+?>
